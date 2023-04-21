facec61c62b27d7382ede7af5cec20688861c770
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-21 03:42:54 +0000

    q

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
   dns_word = var.env == "prod" ? "www" : var.env
   dns_name = var.component == "frontend" ? "${local.dns_word}.${var.dns_domain}" : "${var.component}-${var.env}.${var.dns_domain}"
 
-  parameters = concat([var.component, ], var.parameters)
+  parameters = concat([var.component, "nexus"], var.parameters)
 }
